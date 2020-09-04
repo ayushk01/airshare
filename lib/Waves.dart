b10a8c5002f559a50b989fe48179be156d805047
Prafull thakur <prafullchauhan49@gmail.com>
2020-09-04 23:04:43 +0530

    Color Changed

--- lib/Waves.dart
+++ lib/Waves.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Waves extends StatefulWidget {
   @override
@@ -8,16 +9,30 @@ class Waves extends StatefulWidget {
 
 class _WavesState extends State<Waves> with TickerProviderStateMixin {
   AnimationController _controller;
-
   Animation _colorTween;
   Animation _colorTween2;
   Animation _colorTween3;
   Animation _widthheight;
   Animation _widthheight2;
   Animation _widthheight3;
+  String heroTag = 'assets/ProfilePicture1.jpg';
+  String userName = 'User';
+
+  void setUserState() async {
+    final prefs = await SharedPreferences.getInstance();
+    String name = prefs.getString('name');
+    String picture = prefs.getString('avatar');
+    if (name != null && picture != null) {
+      setState(() {
+        heroTag = picture;
+        userName = name;
+      });
+    }
+  }
 
   @override
   void initState() {
+    setUserState();
     _controller = AnimationController(
       duration: const Duration(seconds: 3),
       vsync: this,
@@ -37,11 +52,11 @@ class _WavesState extends State<Waves> with TickerProviderStateMixin {
     ).animate(_controller);
 
     _widthheight =
-        new Tween<double>(begin: 150.0, end: 100.0).animate(_controller);
+        new Tween<double>(begin: 120.0, end: 100.0).animate(_controller);
     _widthheight2 =
-        new Tween<double>(begin: 250.0, end: 200.0).animate(_controller);
+        new Tween<double>(begin: 220.0, end: 200.0).animate(_controller);
     _widthheight3 =
-        new Tween<double>(begin: 350.0, end: 300.0).animate(_controller);
+        new Tween<double>(begin: 320.0, end: 300.0).animate(_controller);
     super.initState();
   }
 
@@ -65,36 +80,54 @@ class _WavesState extends State<Waves> with TickerProviderStateMixin {
                   body: Container(
                     child: Center(
                       child: Container(
-                        width: _widthheight3.value,
-                        height: _widthheight3.value,
-                        decoration: BoxDecoration(
-                          color: _colorTween.value,
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(180.0)),
-                        ),
-                        child: Center(
-                          child: Container(
-                            width: _widthheight2.value,
-                            height: _widthheight2.value,
-                            decoration: BoxDecoration(
-                              color: _colorTween2.value,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(180.0)),
+                          width: _widthheight3.value,
+                          height: _widthheight3.value,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF99b3ff),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(180.0)),
+                          ),
+                          child: Stack(children: [
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: FlutterLogo(
+                                size: 30,
+                              ),
                             ),
-                            child: Center(
+                            Center(
                               child: Container(
-                                width: _widthheight.value,
-                                height: _widthheight.value,
+                                width: _widthheight2.value,
+                                height: _widthheight2.value,
                                 decoration: BoxDecoration(
-                                  color: _colorTween3.value,
+                                  color: Color(0xFFb3c6ff),
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(180.0)),
                                 ),
+                                child: Center(
+                                  child: Container(
+                                      width: _widthheight.value,
+                                      height: _widthheight.value,
+                                      decoration: BoxDecoration(
+                                        color: Color(0xFFccd9ff),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(180.0)),
+                                      ),
+                                      child: Center(
+                                        child: Container(
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        100.0),
+                                                image: DecorationImage(
+                                                    image: AssetImage(heroTag),
+                                                    fit: BoxFit.cover)),
+                                            height: 50.0,
+                                            width: 50.0),
+                                      )),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      ),
+                          ])),
                     ),
                   )));
         });
